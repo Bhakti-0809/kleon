4780b93e4b9e2307f2ebdb664b47b53f70006dee
Bhakti-0809 <75261550+Bhakti-0809@users.noreply.github.com>
2021-10-11 17:10:59 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+fill("white")
+textSize(15)
+text("C O T N I F O N ",700,150)
+fill("lightblue")
+text("Hint: Performs a particular task!!",700,170)
+
+fill("white")
+textSize(15)
+text("A T A D A S B A ",100,310)
+fill("lightblue")
+text("Hint: Stores all information!!",100,330)
+
+fill("white")
+textSize(15)
+text(" R I V A A L B E ",100,40)
+fill("lightblue")
+text("Hint: Value keeps changing!!",100,70)
+
+}
